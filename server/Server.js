e3703bbf0af351293e27f3dc0f6d74eb2f712abc
Johanna <admin@Johannas-MacBook-Pro.local>
2021-03-07 18:42:52 +0100

    Add new dependencies - helmet & morgan

--- server/Server.js
+++ server/Server.js
@@ -1,15 +1,24 @@
 import express, { request, response } from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const application = express();
+application.use(helmet());
+application.use(morgan('common'));
+
+const checkIfAdmin = (request, response, next) => {
+    console.log("is admin!");
+    next();
+}
 
 application.get('/recipe', (request, response) => {
     response.send("Ditt API anrop gick igenom");
 });
 
-application.get('/throwdice', (request, response) => {
+application.get('/throwdice', checkIfAdmin,  (request, response) => {
     response.send(Math.random().toString());
-})
+});
 
 application.listen(3001, () => {
-    console.log("Server är igång på port " + 3001)
-})
+    console.log("Server är igång på port " + 3001);
+});
